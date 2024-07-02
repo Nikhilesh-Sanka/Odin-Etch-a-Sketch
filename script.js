19da810fa2de874e6da99c76f6a42c2216f0e531
Nikhilesh-Sanka <sankanikhilesh2006.svc@gmail.com>
2024-07-02 21:52:27 +0530

    Completed my First DOM Project

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let gridSize=16;
+let gridColorChangeTo="blue"
 let changeSizeButton=document.querySelector("button");
 let gridContainer=document.querySelector(".container");
 function generateUnitSize (gridSize) {
@@ -24,11 +25,35 @@ generateGrid(gridSize);
 changeSizeButton.addEventListener("click",() => {
     let userInput =Number(getUserInput());
     if(typeof userInput==="number"){
-        generateGrid(userInput);
+        if(userInput<=100){
+            gridSize=userInput;
+            generateGrid(gridSize);
+        }
+        else{
+            generateGrid(gridSize);
+            alert("enter a number and it should be less than or equal to 100");
+        }
     }
     else{
-        return;
+        generateGrid(gridSize);
+        alert("enter a number and it should be less than or equal to 100");
     }
+    gridUnitsNodeList=document.querySelectorAll(".grid-unit");
+    applyEventListener();
 });
+let gridUnitsNodeList=document.querySelectorAll(".grid-unit");
+function changeBackgroundColor(event){
+    event.target.style.backgroundColor=`rgb(0,0,255)`;
+}
+function changeBackgroundColorToNormal (event){
+    event.target.style["background-color"]="white";
+}
+function applyEventListener () {
+    gridUnitsNodeList.forEach((unit)=> {
+        unit.addEventListener("mouseover",changeBackgroundColor);
+        unit.addEventListener("mouseleave",changeBackgroundColorToNormal);
+    });
+}
+applyEventListener();
 
 
